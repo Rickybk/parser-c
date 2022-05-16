3b645ee6a40ae49b606e58523a408cd3bbc90bee
Ricky <dorian.flores@ceis.cs.umss.edu.bo>
2022-05-15 22:35:34 -0400

    Agregados constantes de tamanos

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 
 int main()
 {
+    const int TAMANO_MAX_LINE  = 300;
+    const int TAMANO_MAX_ARRAY = 5000;
     FILE *file =fopen("vsftpd.log", "r");
     if(file == NULL){
         perror("No se pudo abrir el archivo");
         exit(1);
     }
-    char line[100];
+    char line[TAMANO_MAX_LINE];
     while(fgets(line,sizeof(line),file)){
         //print the line
         printf("%s", line);
