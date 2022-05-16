0debff0acf020486ca199396093bd5d3cf839e22
Ricky <dorian.flores@ceis.cs.umss.edu.bo>
2022-05-16 16:28:58 -0400

    Arreglo almacenando el log y mostrandolo correctamente

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main()
 {
     const int TAMANO_MAX_LINE  = 300;
@@ -10,24 +10,36 @@ int main()
     //Definimos el arreglo donde se guardara todo el Log.
     char arregloLog[TAMANO_MAX_ARRAY][TAMANO_MAX_LINE];
     FILE *fileLog =fopen("vsftpd.log", "r");
-    if(fileLog == NULL){
+    if(fileLog == NULL)
+    {
         perror("No se pudo abrir el archivo");
         exit(1);
     }
     //Aca es donde guardamos momentaneamente las lineas del Log.
     char line[TAMANO_MAX_LINE];
-    while(fgets(line,sizeof(line),fileLog)){
+    while(fgets(line,sizeof(line),fileLog))
+    {
         strcpy(arregloLog[contador],line);
         contador++;
         //print the line
         printf("%s", line);
     }
-    printf("El tamano final de nuestro arreglo es: %d",sizeof(arregloLog));
-    /*printf("Aca empezamos a mostrar el arreglo que guardamos");
-    for(i = 0; i< sizeof(arregloLog); i++){
-        if(i%2 == 0){
-            printf("%s",arregloLog[i]);
-        }
-    }*/
+    //printf("El tamano final de nuestro arreglo es: %d\n",sizeof(arregloLog));
+    printf("El numero total de lineas es: %d\n",contador);
+    printf("Aca empezamos a mostrar el arreglo que guardamos\n");
+    for(i = 0; i < contador; i++)
+    {
+
+        printf("%s",arregloLog[i]);
+        /*if(arregloLog[i] != '/0')
+        {
+            if(i%2 == 0)
+            {
+                printf("%s",arregloLog[i]);
+            }
+        } else {
+            break;
+        }*/
+    }
     return 0;
 }
