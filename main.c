fafaf69be27013d4ebb03bf5dc8e84f9763db002
Ricky <dorian.flores@ceis.cs.umss.edu.bo>
2022-05-15 23:06:20 -0400

    Preparando el Array que guardara el log y probando el loop que lo recorre

--- main.c
+++ main.c
@@ -5,15 +5,29 @@ int main()
 {
     const int TAMANO_MAX_LINE  = 300;
     const int TAMANO_MAX_ARRAY = 5000;
-    FILE *file =fopen("vsftpd.log", "r");
-    if(file == NULL){
+    int contador = 0;
+    int i;
+    //Definimos el arreglo donde se guardara todo el Log.
+    char arregloLog[TAMANO_MAX_ARRAY][TAMANO_MAX_LINE];
+    FILE *fileLog =fopen("vsftpd.log", "r");
+    if(fileLog == NULL){
         perror("No se pudo abrir el archivo");
         exit(1);
     }
+    //Aca es donde guardamos momentaneamente las lineas del Log.
     char line[TAMANO_MAX_LINE];
-    while(fgets(line,sizeof(line),file)){
+    while(fgets(line,sizeof(line),fileLog)){
+        strcpy(arregloLog[contador],line);
+        contador++;
         //print the line
         printf("%s", line);
+    }
+
+    printf("Aca empezamos a mostrar el arreglo que guardamos");
+    for(i = 0; i< sizeof(arregloLog); i++){
+        if(i%2 == 0){
+            printf("%s",arregloLog[i]);
+        }
     }
     return 0;
 }
